f9eda2a92f581793d0a5f5f148d1a99ca27cff0f
Maksym Lopatin <tailor_striped_0m@icloud.com>
2024-06-30 15:06:52 +0300

    Initial commit

---
+++ ynab_openapi/api/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+# import apis into api package
+from ynab_openapi.api.accounts_api import AccountsApi
+from ynab_openapi.api.budgets_api import BudgetsApi
+from ynab_openapi.api.categories_api import CategoriesApi
+from ynab_openapi.api.months_api import MonthsApi
+from ynab_openapi.api.payee_locations_api import PayeeLocationsApi
+from ynab_openapi.api.payees_api import PayeesApi
+from ynab_openapi.api.scheduled_transactions_api import ScheduledTransactionsApi
+from ynab_openapi.api.transactions_api import TransactionsApi
+from ynab_openapi.api.user_api import UserApi
+
